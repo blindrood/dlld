6e60c79acaf21a1a035beaee3fc38c2442a3386e
blindrood <48821393+blindrood@users.noreply.github.com>
2019-04-11 13:38:29 +0200

    Update types.go

--- driver/types.go
+++ driver/types.go
@@ -5,15 +5,10 @@ import (
 )
 
 type jsonLogLine struct {
-	ContainerId   string            `json:"container_id"`
-	ContainerName string            `json:"container_name"`
-	StackName     string            `json:"stack_name"`
-	ServiceName   string            `json:"service_name"`
-	ImageId       string            `json:"image_id"`
-	ImageName     string            `json:"image_name"`
-	Command       string            `json:"command"`
-	Tag           string            `json:"tag"`
-	Extra         map[string]string `json:"extra"`
+	Docker_ContainerId   string            `json:"container_id"`
+	Docker_ContainerName string            `json:"container_name"`
+	Docker_ImageId       string            `json:"image_id"`
+	Docker_ImageName     string            `json:"image_name"`
 	Host          string            `json:"host"`
 	Timestamp     time.Time         `json:"timestamp"`
 }
